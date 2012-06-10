066b2c43807ca3a58eb62d4c0aff44cb2ea108a5
Paul Goldbaum <paul@github>
2012-06-10 16:30:49 +0200

    Added login and registration forms

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,44 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import views._
+import models._
 
 object Application extends Controller {
   
+  val loginForm = Form(
+      mapping(
+          "username" -> text,
+          "password" -> text
+      )(User.apply)(User.unapply)
+  )
+      
+  val registrationForm = Form(
+      mapping(
+          "username" -> text,
+          "password" -> text
+      )(User.apply)(User.unapply)
+  )
+      
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(html.index(loginForm, registrationForm))
   }
+  
+  def registerUser = Action { implicit request =>
+    registrationForm.bindFromRequest.fold(
+        formWithErrors => BadRequest(views.html.index(loginForm, formWithErrors)),
+        {
+          case User(username, password) =>  {
+	          User.create(username, password)
+	          Redirect(routes.Application.index())
+	        }
+        }
+      )
+    }
+  
+  
   
 }
---
+++ app/models/User.scala
@@ -0,0 +1,9 @@
+package models
+
+case class User (username: String, password: String)
+
+object User {
+  
+  def create(username: String, password: String) {}
+  
+}
